102cf1ac7479b698b6110aafb636561ba557c9ef
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-08-27 23:17:00 +0545

    add: auth 1/2

---
+++ apps/admin/src/server/modules/auth/procedure/procedure.ts
@@ -0,0 +1,66 @@
+import { router, publicProcedure } from '~/server/trpc';
+import { TRPCError } from '@trpc/server';
+import { prisma } from '~/server/prisma';
+import registerUserSchema from '../entities/requests/registerRequest';
+import loginUserSchema from '../entities/requests/loginRequest';
+import AuthController from '../controllers/authController';
+import { TokenResponse } from '../entities/responses/tokenResponse';
+
+const registerProcedure = publicProcedure
+    .input(registerUserSchema)
+    .mutation(async ({ input }) => {
+      console.log(input.email, input.password);
+      const registered = await AuthController.registerUser(
+        input.email,
+        input.password,
+        prisma,
+      );
+      if (registered.isErr) {
+        throw new TRPCError({
+          code: 'CONFLICT',
+          message: registered.error.message,
+          cause: registered.error.field,
+        });
+      }
+      const token = await AuthController.generateToken(registered.value.id);
+      return {
+        authToken: token,
+      } as TokenResponse;
+})
+
+const loginProcedure =  publicProcedure.input(loginUserSchema).query(async ({ input }) => {
+    const loggedIn = await AuthController.loginUser(
+      input.email,
+      input.password,
+      prisma,
+    );
+    if (loggedIn.isErr) {
+      throw new TRPCError({
+        code: 'FORBIDDEN',
+        message: loggedIn.error.message,
+        cause: loggedIn.error.field,
+      });
+    }
+    const token = await AuthController.generateToken(loggedIn.value.id);
+    return {
+      authToken: token,
+    } as TokenResponse;
+})
+const fetchSelfProcedure = {
+
+}
+
+const isAdminProcedure = {
+
+}
+
+
+
+
+export {
+    registerProcedure,
+    loginProcedure,
+    fetchSelfProcedure,
+    isAdminProcedure
+
+}
--- apps/admin/src/server/routers/_app.ts
+++ apps/admin/src/server/routers/_app.ts
@@ -1,13 +1,14 @@
 /**
  * This file contains the root router of your tRPC-backend
  */
+import authRouter from '../modules/auth/router/authRouter';
 import { createCallerFactory, publicProcedure, router } from '../trpc';
 import { postRouter } from './post';
 
 export const appRouter = router({
   healthcheck: publicProcedure.query(() => 'yay!'),
 
-  post: postRouter,
+  auth:authRouter
 });
 
 export const createCaller = createCallerFactory(appRouter);
