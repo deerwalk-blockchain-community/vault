c5736c10448761c4381c6de720b008205d58b43b
chrisdvkta <chrisdevkota77@gmail.com>
2024-08-28 23:13:06 +0545

    add : registration and login

--- apps/frontend/src/server/modules/auth/entities/requests/loginRequest.ts
+++ apps/frontend/src/server/modules/auth/entities/requests/loginRequest.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
-const emailRegex = new RegExp('^[w-.]+@([w-]+.)+[w-]{2,4}$');
+// const emailRegex = new RegExp('^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$');
 
 const loginUserSchema = z.object({
-  email: z.string().regex(emailRegex, 'Please provide a valid email!'),
+  email: z.string(),
   password: z.string().min(8, 'Password Should be at least 8 character long!'),
 });
 
--- apps/frontend/src/server/modules/auth/entities/requests/registerRequest.ts
+++ apps/frontend/src/server/modules/auth/entities/requests/registerRequest.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 
-const emailRegex = new RegExp('^[w-.]+@([w-]+.)+[w-]{2,4}$');
+// const emailRegex = new RegExp('^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$');
 
 const registerUserSchema = z
   .object({
-    email: z.string().regex(emailRegex, 'Please provide a valid email!'),
+    email: z.string(),
     password: z
       .string()
       .min(8, 'Password Should be at least 8 character long!'),
