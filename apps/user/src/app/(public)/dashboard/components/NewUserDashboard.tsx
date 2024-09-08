a362f342e5044b79ef273f6f56ca753a4528da6a
Samrat Nakarmi <samratnakarmi1@gmail.com>
2024-09-08 13:41:52 +0545

    chore

--- apps/user/src/app/(public)/dashboard/components/NewUserDashboard.tsx
+++ apps/user/src/app/(public)/dashboard/components/NewUserDashboard.tsx
@@ -8,7 +8,7 @@ const NewUserDashboard = () => {
     <div className="bg-[#1a1b1d] w-full mt-8 mx-auto rounded-xl">
       <div className="w-[90%] mx-auto py-8">
         <h1 className="text-center text-2xl font-semibold ">
-          You Haven't Filled Out the KYC Yet
+          You Haven&apos;t Filled Out the KYC Yet
         </h1>
         <div className="text-center mt-8">
           <Button className="bg-[#242424]" onClick={() => router.push("/form")}>
